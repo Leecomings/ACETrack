90aadaeb643f2313394cfb5ed0b5c70716272f4e
Leecoming <15954915+leecoming@user.noreply.gitee.com>
2026-04-16 21:38:30 +0800

    添加GeneratedPluginRegistrant文件，解决Undefined symbol错误

---
+++ iostennis/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,12 @@
+//  Generated file. Do not edit.
+//  iostennis
+//
+//  Created by Flutter on 2026-04-16.
+
+#import "GeneratedPluginRegistrant.h"
+
+@implementation GeneratedPluginRegistrant
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  // Empty implementation for now
+}
+@end
